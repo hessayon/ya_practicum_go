0824839c17761ea3784027993f70ae577c2d9d1b
hessayon <imaggi99@mail.ru>
2023-11-12 19:49:20 +0300

    Create storage interface

--- internal/storage/urlsstorage.go
+++ internal/storage/urlsstorage.go
@@ -6,28 +6,74 @@ import (
 	"os"
 )
 
+type URLStorage interface {
+	Save(urlData *URLData) (err error)
+	Get(shortURL string) (value string, ok bool)
+	Close()
+
+}
+
 type URLData struct {
 	UUID        string `json:"uuid"`
 	ShortURL    string `json:"short_url"`
 	OriginalURL string `json:"original_url"`
 }
 
-type URLStorageSaver struct {
+type URLStorageFileSaver struct {
 	file    *os.File
 	encoder *json.Encoder
 }
 
-type URLStorage struct {
+type LocalURLStorage struct {
 	Store    map[string]string
 	filename string
-	saver    *URLStorageSaver
+	saver    *URLStorageFileSaver
 }
 
-var Storage *URLStorage
+var Storage URLStorage
 
-func NewURLStorage(filename string) (*URLStorage, error) {
 
-	storage := &URLStorage{
+func newStorageSaver(filename string) (*URLStorageFileSaver, error) {
+
+	if filename == "" {
+		return nil, nil
+	}
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	return &URLStorageFileSaver{
+		file:    file,
+		encoder: json.NewEncoder(file),
+	}, nil
+}
+
+func (storage *LocalURLStorage) Save(urlData *URLData) error {
+	storage.Store[urlData.ShortURL] = urlData.OriginalURL
+	if(storage.saver != nil){
+		return storage.saver.encoder.Encode(urlData)
+	}
+	return nil
+}
+
+
+func (storage *LocalURLStorage) Get(shortURL string) (string, bool) {
+ 	fullURL, found := storage.Store[shortURL]
+	return fullURL, found
+}
+
+
+func (storage *LocalURLStorage) Close() {
+	if(storage.saver != nil){
+		storage.saver.file.Close()
+	}
+}
+
+
+func NewURLStorage(filename string) (URLStorage, error) {
+
+	storage := &LocalURLStorage{
 		Store:    make(map[string]string),
 		filename: filename,
 		saver: nil,
@@ -60,41 +106,4 @@ func NewURLStorage(filename string) (*URLStorage, error) {
 	}
 	storage.saver = storageSaver
 	return storage, nil
-}
-
-func newStorageSaver(filename string) (*URLStorageSaver, error) {
-
-	if filename == "" {
-		return nil, nil
-	}
-	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		return nil, err
-	}
-
-	return &URLStorageSaver{
-		file:    file,
-		encoder: json.NewEncoder(file),
-	}, nil
-}
-
-func (storage *URLStorage) Save(urlData *URLData) error {
-	storage.Store[urlData.ShortURL] = urlData.OriginalURL
-	if(storage.saver != nil){
-		return storage.saver.encoder.Encode(urlData)
-	}
-	return nil
-}
-
-
-func (storage *URLStorage) Get(shortURL string) (string, bool) {
- 	fullURL, found := storage.Store[shortURL]
-	return fullURL, found
-}
-
-
-func (storage *URLStorage) Close() {
-	if(storage.saver != nil){
-		storage.saver.file.Close()
-	}
 }
