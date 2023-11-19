e45e6ba6d62f2ee54ce08b3d094f6f406cd3bbeb
hessayon <imaggi99@mail.ru>
2023-11-19 22:20:13 +0300

    Add new method to URLStorage interface

--- internal/storage/urlsstorage.go
+++ internal/storage/urlsstorage.go
@@ -6,6 +6,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	"os"
 
@@ -19,11 +20,12 @@ type URLStorage interface {
 	SaveBatch(urlsBatch []*URLData) (err error)
 	GetOriginalURL(shortURL string) (value string, ok bool)
 	GetShortURL(originalURL string) (value string, ok bool)
+	GetURLsByUserID(userID string) (value []URLData, err error)
 	Close()
 }
 
 type URLData struct {
-	UUID        string `json:"uuid"`
+	UUID        string `json:"uuid,omitempty"`
 	ShortURL    string `json:"short_url"`
 	OriginalURL string `json:"original_url"`
 }
@@ -38,6 +40,7 @@ type URLStorageFileSaver struct {
 type LocalURLStorage struct {
 	ShortToOrig map[string]string
 	OrigToShort map[string]string
+	UUIDToData map[string][]URLData
 	filename    string
 	saver       *URLStorageFileSaver
 }
@@ -76,6 +79,7 @@ func (storage *LocalURLStorage) Save(urlData *URLData) error {
 	}
 	storage.ShortToOrig[urlData.ShortURL] = urlData.OriginalURL
 	storage.OrigToShort[urlData.OriginalURL] = urlData.ShortURL
+	storage.UUIDToData[urlData.UUID] = append(storage.UUIDToData[urlData.UUID], *urlData)
 	if storage.saver != nil {
 		return storage.saver.encoder.Encode(urlData)
 	}
@@ -108,6 +112,15 @@ func (storage *LocalURLStorage) Close() {
 	}
 }
 
+func (storage *LocalURLStorage) GetURLsByUserID(userID string) ([]URLData, error) {
+	resList, found := storage.UUIDToData[userID]
+	if !found {
+		errMsg := fmt.Sprintf("Not found data for %s", userID)
+		return nil, errors.New(errMsg)
+	}
+	return resList, nil
+}
+
 //--------------------------------------------------------------------
 
 func (storage *URLDBStorage) createTable() error {
@@ -115,6 +128,7 @@ func (storage *URLDBStorage) createTable() error {
 	CREATE TABLE urls (
 		short_url varchar NOT NULL,
 		full_url varchar NOT NULL,
+		uuid varchar NOT NULL,
 		CONSTRAINT urls_pk PRIMARY KEY (full_url)
 	);`
 	_, err := storage.DB.ExecContext(context.Background(), query)
@@ -122,7 +136,7 @@ func (storage *URLDBStorage) createTable() error {
 }
 
 func (storage *URLDBStorage) Save(urlData *URLData) error {
-	query := "INSERT INTO urls VALUES ($1, $2);"
+	query := "INSERT INTO urls VALUES ($1, $2, $3);"
 	_, err := storage.DB.ExecContext(context.Background(), query, urlData.ShortURL, urlData.OriginalURL)
 	if err != nil {
 		var pgErr *pgconn.PgError
@@ -132,7 +146,7 @@ func (storage *URLDBStorage) Save(urlData *URLData) error {
 			if err != nil {
 				return err
 			}
-			_, err := storage.DB.ExecContext(context.Background(), query, urlData.ShortURL, urlData.OriginalURL)
+			_, err := storage.DB.ExecContext(context.Background(), query, urlData.ShortURL, urlData.OriginalURL, urlData.UUID)
 			return err
 		} else if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.UniqueViolation {
 			err = ErrConflict
@@ -146,7 +160,7 @@ func (storage *URLDBStorage) Save(urlData *URLData) error {
 }
 
 func (storage *URLDBStorage) SaveBatch(urlsBatch []*URLData) error {
-	query := "INSERT INTO urls VALUES ($1, $2);"
+	query := "INSERT INTO urls VALUES ($1, $2, $3);"
 	tx, err := storage.DB.Begin()
 	if err != nil {
 		return err
@@ -159,7 +173,7 @@ func (storage *URLDBStorage) SaveBatch(urlsBatch []*URLData) error {
 	}
 	defer stmt.Close()
 	for _, data := range urlsBatch {
-		_, err := stmt.ExecContext(ctx, data.ShortURL, data.OriginalURL)
+		_, err := stmt.ExecContext(ctx, data.ShortURL, data.OriginalURL, data.UUID)
 		if err != nil {
 			var pgErr *pgconn.PgError
 			// если не найдена такая таблица, то пробуем создать таблицу
@@ -168,7 +182,7 @@ func (storage *URLDBStorage) SaveBatch(urlsBatch []*URLData) error {
 				if err != nil {
 					return err
 				}
-				_, err := stmt.ExecContext(ctx, data.ShortURL, data.OriginalURL)
+				_, err := stmt.ExecContext(ctx, data.ShortURL, data.OriginalURL, data.UUID)
 				if err != nil {
 					return err
 				}
@@ -207,6 +221,31 @@ func (storage *URLDBStorage) GetShortURL(originalURL string) (string, bool) {
 func (storage *URLDBStorage) Close() {
 	storage.DB.Close()
 }
+
+func (storage *URLDBStorage) GetURLsByUserID(userID string) ([]URLData, error) {
+	resList := make([]URLData, 0)
+	query := "SELECT short_url, full_url FROM urls WHERE uuid = $1"
+	rows, err := storage.DB.QueryContext(context.Background(), query, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var urlData URLData
+		err = rows.Scan(&urlData.ShortURL, &urlData.OriginalURL)
+		if err != nil {
+				return nil, err
+		}
+		resList = append(resList, urlData)	
+	}	
+
+
+	err = rows.Err()
+	if err != nil {
+			return nil, err
+	}
+	return resList, nil
+}
 //--------------------------------------------------------------------
 
 
@@ -231,6 +270,7 @@ func newLocalURLStorage(filename string) (*LocalURLStorage, error) {
 	storage := &LocalURLStorage{
 		ShortToOrig: make(map[string]string),
 		OrigToShort: make(map[string]string),
+		UUIDToData: make(map[string][]URLData),
 		filename:    filename,
 		saver:       nil,
 	}
@@ -252,6 +292,8 @@ func newLocalURLStorage(filename string) (*LocalURLStorage, error) {
 			return nil, err
 		}
 		storage.ShortToOrig[urlData.ShortURL] = urlData.OriginalURL
+		storage.OrigToShort[urlData.OriginalURL] = urlData.ShortURL
+		storage.UUIDToData[urlData.UUID] = append(storage.UUIDToData[urlData.UUID], urlData)
 	}
 	if err := scanner.Err(); err != nil {
 		return nil, err
